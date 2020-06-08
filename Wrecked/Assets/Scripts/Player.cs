4a26d85b4c0ec13aa87249b56eec56aac50d4fb6
Tom Downly <to.downly@gmail.com>
2020-06-08 23:49:45 +0200

    Zakladni Data :)

---
+++ Wrecked/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public int camera;
+
+
+    //FPS
+    public Transform fps;
+
+	// Use this for initialization
+	void Start () {
+        camera = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
