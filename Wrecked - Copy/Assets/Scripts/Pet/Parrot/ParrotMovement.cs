c166f5a4161deacd8d4dc0d839a51808b100a3b7
Tom Downly <to.downly@gmail.com>
2020-06-09 15:01:03 +0200

    Bug Fixes + Stable Character Controller

---
+++ Wrecked - Copy/Assets/Scripts/Pet/Parrot/ParrotMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ParrotMovement : MonoBehaviour
+{
+
+    public Transform player;
+    public float movementSpeed;
+    public float currentY;
+
+    public NavMeshAgent agent;
+
+    public int currentX;
+    public int currentZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Physics.autoSimulation = false;
+
+        agent.SetDestination(new Vector3(player.position.x - (int) 2.5, player.position.y - (int) 2.5, player.position.z - (int) 2.5));
+    }
+}
---
+++ Wrecked/Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour
+{
+
+    const float locomotionAnimationSmoothTime = 1f;
+
+    public Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetFloat("Speed", .5f, locomotionAnimationSmoothTime, Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.W))
+        {
+            animator.SetFloat("Speed", 1f, locomotionAnimationSmoothTime, Time.deltaTime);
+        }
+        if (!Input.GetKey(KeyCode.W))
+        {
+            animator.SetFloat("Speed", 0f, locomotionAnimationSmoothTime, Time.deltaTime);
+        }
+    }
+}
